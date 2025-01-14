0294fe6a714b60def4352cae3f1cc22f06f8c7a7
mintaek22 <biba99@naver.com>
2025-01-14 14:38:49 +0900

    chore: db-manager redis 세팅

---
+++ DBManager/src/config/redis/redis.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import Redis from 'ioredis';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class RedisService {
+  private readonly redisClient: Redis;
+
+  constructor(private readonly configService: ConfigService) {
+    this.redisClient = new Redis({
+      host: this.configService.get<string>('REDIS_HOST'),
+      port: this.configService.get<number>('REDIS_PORT'),
+    });
+  }
+
+  async set(key: string, value: string, ttl?: number): Promise<void> {
+    if (ttl) {
+      await this.redisClient.set(key, value, 'EX', ttl);
+    } else {
+      await this.redisClient.set(key, value);
+    }
+  }
+
+  async get(key: string): Promise<string | null> {
+    return this.redisClient.get(key);
+  }
+
+  async del(key: string): Promise<number> {
+    return this.redisClient.del(key);
+  }
+
+  async exists(key: string): Promise<number> {
+    return this.redisClient.exists(key);
+  }
+}
