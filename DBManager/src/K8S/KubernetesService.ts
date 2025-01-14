f44f5024caf38d2719e0f46d3805cacee5c1c90d
syj <syj010820@naver.com>
2025-01-14 15:31:01 +0900

    feat: pod 생성, 삭제 이벤트 구독

--- DBManager/src/K8S/KubernetesService.ts
+++ DBManager/src/K8S/KubernetesService.ts
@@ -5,12 +5,34 @@ import * as k8s from '@kubernetes/client-node';
 export class KubernetesService implements OnModuleInit {
   private podCnt = 0;
   private k8sApi;
+  private k8sWatch : k8s.Watch;
   private namespace = 'default';
 
   async onModuleInit() {
     const kc = new k8s.KubeConfig();
     kc.loadFromDefault();
     this.k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+    this.k8sWatch = new k8s.Watch(kc);
+    this.startWatchPod();
+  }
+
+  startWatchPod() {
+    const path = `/api/v1/namespaces/${this.namespace}/pods`;
+    const queryParams = { allowWatchBookmarks: true };
+    const handlePodEvent = (type : String, apiObj: any, watchObj: any) => {
+      if (type === 'ADDED') {
+          console.log('Watch Created Pod');
+          console.log(watchObj.object.metadata.name);
+      } else if (type === 'DELETED') {
+          console.log('Watch Deleted Pod');
+          console.log(watchObj.object.metadata.name);
+      }
+    };
+    const handleError = (err: any) => {
+      console.log(err);
+    };
+
+    const req = this.k8sWatch.watch(path, queryParams, handlePodEvent, handleError);
   }
 
   async createPod() {
