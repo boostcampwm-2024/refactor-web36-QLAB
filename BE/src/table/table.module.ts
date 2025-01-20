bfe3bdcd2a8cd4dc17a7dbd564a4023db5fa24d9
Hoons97 <wkdtmdgnsqn@naver.com>
2025-01-20 14:22:58 +0900

    fix : table 모듈에 redis 모듈 의존성 추가

--- BE/src/table/table.module.ts
+++ BE/src/table/table.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TableService } from './table.service';
 import { TableController } from './table.controller';
 import { UserDBModule } from '../config/query-database/user-db.moudle';
+import { RedisModule } from 'src/config/redis/redis.module';
 
 @Module({
-  imports: [UserDBModule],
+  imports: [UserDBModule, RedisModule],
   controllers: [TableController],
   providers: [TableService],
   exports: [TableService],
