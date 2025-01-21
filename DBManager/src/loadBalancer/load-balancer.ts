32ec2182054d76a9dbec00b1c8f3e704a7dfe3d5
syj <syj010820@naver.com>
2025-01-21 13:56:05 +0900

    fix: minpod 찾는 로직 수정

--- DBManager/src/loadBalancer/load-balancer.ts
+++ DBManager/src/loadBalancer/load-balancer.ts
@@ -5,16 +5,18 @@ import { RedisService } from "src/config/redis/redis.service";
 export class LoadBalancer {
   constructor(private readonly redisService: RedisService) {}
   async allocate(sessionId: string) {
-    const allPods = await this.redisService.keysPod();
-    const podList = [];
+    const allPods = await this.redisService.keysPod()
+
+    let minActiveUser = Infinity;
+    let selectedPod = '';
 
     for (const podName of allPods) {
-      const activeUser = await this.redisService.hgetPod(podName, 'activeUser');
-      podList.push({ podName, activeUser });
+      const activeUser = Number(await this.redisService.hgetPod(podName, 'activeUser'));
+      if (activeUser < minActiveUser) {
+        minActiveUser = activeUser;
+        selectedPod = podName;
+      }
     }
-    podList.sort((a, b) => a.activeUser - b.activeUser);
-
-    const selectedPod = podList[0].podName;
     const selectedPodIp = await this.redisService.hgetPod(selectedPod, 'podIp');
 
     await this.redisService.hsetSession(sessionId, 'pod', selectedPod);
