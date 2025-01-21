dc9d7a28987dd9857f4d861c67148349deafc1f0
Hoon <wkdtmdgnsqn@naver.com>
2025-01-21 17:29:48 +0900

    fix : 잘못된 주석 삭제

--- DBManager/ecosystem.config.js
+++ DBManager/ecosystem.config.js
@@ -3,9 +3,9 @@ module.exports = {
     {
       name: 'nest-server',
       script: 'dist/main.js', // NestJS 서버의 진입 파일
-      instances: 1, // 클러스터 모드: CPU 코어 수만큼 프로세스 실행
-      exec_mode: 'fork', // 클러스터 모드
-      watch: false, // 파일 변경 감지 후 재시작
+      instances: 1, 
+      exec_mode: 'fork', 
+      watch: false,
       env: {
         NODE_ENV: 'development',
       },
