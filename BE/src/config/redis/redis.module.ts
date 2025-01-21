aef707a44170a65a159d803bc4968e2feec96582
Hoon <wkdtmdgnsqn@naver.com>
2025-01-21 13:41:57 +0900

    fix : 안쓰는 configModule 임포트 삭제

--- BE/src/config/redis/redis.module.ts
+++ BE/src/config/redis/redis.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { UserDBModule } from '../query-database/user-db.moudle';
-import { ConfigModule } from '@nestjs/config';
 import { RedisProviders } from './redis.providers';
 
 @Module({
