9a61c39075d1d6195996a6ce3788e955a0a8eb8f
mintaek22 <biba99@naver.com>
2025-01-22 17:28:26 +0900

    fix:dbmanager activeuser module 활성화

--- DBManager/src/app.module.ts
+++ DBManager/src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { K8SApiModule } from './k8s/K8SApi.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { SessionEventModule } from './session/session-event.module';
+import { ActiveUserModule } from './active-user/active-user.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { SessionEventModule } from './session/session-event.module';
     ScheduleModule.forRoot(),
     K8SApiModule,
     SessionEventModule,
+    ActiveUserModule,
   ],
   controllers: [AppController],
   providers: [],
