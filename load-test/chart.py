e0d9efc7e7564f0197cfede9cc61ff344a3131fa
mintaek22 <biba99@naver.com>
2025-01-31 11:48:54 +0900

    fix: locust 부하테스트 차트 생성 코드 추가

--- load-test/chart.py
+++ load-test/chart.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 import re
+import numpy as np
+from matplotlib import rc
+import statistics
+
+rc('font', family='Apple SD Gothic Neo')
 
 # 파일 경로 설정
-file_path = "locust_output.txt"
+file_path = "순차처리X인터벌줄이기.txt"
 
 # 초 데이터를 추출하여 ms로 변환
 response_times_ms = []
@@ -12,17 +17,26 @@ with open(file_path, "r") as file:
         match = re.search(r"\((\d+\.\d+) sec\)", line)
         if match:
             seconds = float(match.group(1))  # 초 값을 추출
-            response_times_ms.append(seconds * 1000)  # ms로 변환
+            response_times_ms.append(seconds)  # ms로 변환
 
 # X축: 요청의 인덱스 (1부터 시작)
 request_indices = list(range(1, len(response_times_ms) + 1))
+bins = [0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2]
+
+counts, _ = np.histogram(response_times_ms, bins=bins)
+
+# 비율 계산
+percentages = counts / sum(counts) * 100
+
+print('평균',statistics.mean(response_times_ms))
+
 
 # 차트 생성
-plt.figure(figsize=(12, 6))
-plt.plot(request_indices, response_times_ms, label="Query Execution Time", marker="o", markersize=1)
-plt.title("Query Execution Time Analysis")
-plt.xlabel("Number of Requests")
-plt.ylabel("Query Execution Time (ms)")
-plt.grid(True)
-plt.legend()
+labels = [f"{bins[i]}~{bins[i + 1]}" for i in range(len(bins) - 1)]
+plt.bar(labels, percentages, alpha=0.85,width=0.4)
+plt.title("쿼리 실행 시간 분포", fontsize=18, fontweight='bold',pad=20)
+plt.xlabel("실행 시간 범위 (초)", fontsize=12, labelpad=10)
+plt.ylabel("비율 (%)", fontsize=12, labelpad=5)
+plt.yticks(range(0, 25, 5))
+
 plt.show()
--- load-test/locustfile.py
+++ load-test/locustfile.py
@@ -1,8 +1,7 @@
 from locust import HttpUser, task, between
 
 class User(HttpUser):
-    # 대기 시간 설정 (10초에서 20초 사이)
-    wait_time = between(5, 10)
+    wait_time = between(0,0.5)
 
     def on_start(self):
         # 첫 번째 API 호출 결과를 저장할 변수
@@ -16,7 +15,7 @@ class User(HttpUser):
     def call_apis(self):
         if self.shellId:
             # 쿼리 정의
-            create_table_query = "SELECT BENCHMARK(20000000, POW(2, 1));"
+            create_table_query = "SELECT BENCHMARK(200000000, POW(2, 1));"
 
             # 두 번째 API 호출
             response2 = self.client.post(
@@ -28,7 +27,7 @@ class User(HttpUser):
             if response2.status_code == 201:
                 # 응답 데이터 파일에 저장
                 data_to_write = response2.json().get('data', {}).get('text', '')
-                User.log_to_file("locust_output.txt", data_to_write)
+                User.log_to_file("순차처리쿼리무거운거.txt", data_to_write)
 
     @staticmethod
     def log_to_file(filename, data):
